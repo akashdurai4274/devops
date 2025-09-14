cb562e3d8305e1c17fe0d0f8b25f0d1f29534527
akashdurai4274 <akashdurai4274@gmail.com>
2025-09-14 15:56:25 +0530

    Port Configuration

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
+const PORT = process.env.PORT ||| 3000;
 
 app.get("/", (req, res) => {
     res.json("Devops Course");
 })
 
-app.listen(3000, () => {
-    console.log(`App listens in the port 3000`);
+app.listen(PORT, () => {
+    console.log(`App listens in the port PORT`);
 })
