59691947771ef2e45cb8058f2853b9b661c2e96c
akashdurai4274 <akashdurai4274@gmail.com>
2025-09-14 16:20:10 +0530

    Add CI Pipeline with Github Actions

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT ||| 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json("Devops Course");
+    res.send("Welcome to the Devops Course");
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listens in the port PORT`);
 })
+
+module.exports = { app, server }
