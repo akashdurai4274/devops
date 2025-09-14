b70aaccd633c04b7e95e5f164493f6ee3d7a362f
akashdurai4274 <akashdurai4274@gmail.com>
2025-09-14 17:55:36 +0530

    Test CI workflows

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const name = "Akasj"
 app.get("/", (req, res) => {
-    res.send("Welcome to the Devops Course");
+    res.send(`Welcome to the Devops Course ${name}`);
 })
 
 const server = app.listen(PORT, () => {
     console.log(`App listens in the port PORT`);
 })
 
-module.exports = { app, server }
+module.exports = { app, server, name }
